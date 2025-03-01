e158a86c1e00734fc2eb279624ba8b0694830311
Kim Dudenhöfer <kidu2400@student.miun.se>
2025-03-01 12:11:05 +0100

    Läst in API med kursdata och sorterat på antal sökanden, samt delat upp datan på kurser och program.

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,16 @@ const closeBtn = document.getElementById("closeMenu");
 const mainMenu = document.getElementById("navContainer");
 const loadBtn = document.getElementById("btn");
 const loadIcon = document.getElementById("loadIcon");
+const programs = document.getElementById("programChart");
+
+window.onload = retrieveData();
 
 //Checking value of null and adding eventlisteners if variable exists
 openBtn.addEventListener('click', displayMenu);
 closeBtn.addEventListener('click', displayMenu);
-loadBtn.addEventListener('click', displayLoad);
+if(loadBtn !== null) {
+    loadBtn.addEventListener('click', displayLoad);
+}
 
 //Toggle main menu i mobile-version
 function displayMenu() {
@@ -33,4 +38,81 @@ function displayLoad() {
     } else {
         loadIcon.style.visibility = "hidden";
     }
+}
+
+//Fetching API with try/catch och async/await
+async function getData() {
+    try {
+        const response = await fetch('https://studenter.miun.se/~mallar/dt211g/');
+
+        if(!response.ok) {
+            throw new Error('Ett fel har uppträtt. Felaktigt svar från servern.');
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('Fel har uppstått:', error.message);
+    }
+
+}
+
+//Sorting data after applicantsTotal and sending to function organise
+async function retrieveData() {
+    const data = await getData();
+
+    const sortedData = data.sort((a,b) => b.applicantsTotal - a.applicantsTotal);
+   
+    organiseData(sortedData);
+}
+
+//Organising data after type
+async function organiseData(sentData) {
+    const allPrograms = [];
+    const allCourses = [];
+
+    for(let i = 0; i < sentData.length; i++) {
+
+        if(sentData[i].type === "Program") {
+
+            allPrograms.push(sentData[i]);
+
+        } else if(sentData[i].type === "Kurs") {
+
+            allCourses.push(sentData[i]);
+
+        }
+    }
+    
+    topPrograms(allPrograms);
+    topCourses(allCourses);
+}
+
+//Selecting top 6 values from all programs
+async function topPrograms(allPrograms) {
+    console.log(allPrograms);
+}
+
+//Selecting top 6 values from all courses
+async function topCourses(allCourses) {
+    console.log(allCourses);
+}
+
+//Create chart
+if(programs != null) {
+    new Chart(programs, {
+        type: 'bar',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Top 6 populäraste kurserna',
+                data: [],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            backgroundColor: ['grey','black','darkred']
+        }
+    });
 }
